d737a0f5f26a25bcf0b582b364b928b52fa8130f
Hack Reactor Student <student@hackreactor.com>
2017-08-03 20:09:00 -0700

    Database has users model

---
+++ database/models/users.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+    var Users = sequelize.define('users', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        firstname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+        lastname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return Users;
+};
