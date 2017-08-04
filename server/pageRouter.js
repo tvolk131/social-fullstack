96b6da46f9e20b809d9e9de675db8cca1d14ee94
Hack Reactor Student <student@hackreactor.com>
2017-08-04 15:42:24 -0700

    Root directory of website is auth locked

--- server/pageRouter.js
+++ server/pageRouter.js
@@ -2,6 +2,16 @@ var router = require('express').Router();
 
 const passport = require('passport');
 
+var isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+router.get('/', isLoggedIn);
+
 router.post('/login', passport.authenticate('local-signup', {
   successRedirect: '/',
   failureRedirect: '/login'
@@ -11,5 +21,11 @@ router.post('/signup', passport.authenticate('local-signup', {
   successRedirect: '/login',
   failureRedirect: '/signup'
 }));
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
