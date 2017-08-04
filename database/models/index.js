3d88f1ca89272e933ed796c274d711c8048d291c
Hack Reactor Student <student@hackreactor.com>
2017-08-04 13:16:14 -0700

    Database schemas all reside in one file

--- database/models/index.js
+++ database/models/index.js
@@ -8,22 +8,80 @@ var config = require(path.join(__dirname, '../../config/config.json'))[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
-fs.readdirSync(__dirname)
-.filter((file) => {
-    return (file.indexOf('.') != 0) && (file != 'index.js');
-})
-.forEach((file) => {
-    var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-});
-
-Object.keys(db).forEach((modelName) => {
-    if ('associate' in db[modelName]) {
-        db[modelName].associate(db);
-    }
-});
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+// ------- //
+// SCHEMAS //
+// ------- //
+
+// USERS SCHEMA
+var Users = sequelize.define('users', {
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    firstname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
+    lastname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+db.users = Users;
+
+// MESSAGES SCHEMA
+var Messages = sequelize.define('messages', {
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    text: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+Messages.belongsTo(Users, {
+    foreignKey: 'sender_id'
+});
+Messages.belongsTo(Users, {
+    foreignKey: 'recipient_id'
+});
+db.messages = Messages;
+
+// POSTS SCHEMA
+var Posts = sequelize.define('posts', {
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
+    text: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+Posts.belongsTo(Users, {
+    foreignKey: 'user_id'
+});
+db.posts = Posts;
+
+
+
+
 
 module.exports = db;
