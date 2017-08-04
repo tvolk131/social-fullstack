1ca63abc382656dc44195d5678a6db440c704f5b
Hack Reactor Student <student@hackreactor.com>
2017-08-03 18:20:30 -0700

    Server uses mysql (not fully implemented)

--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
 const db = require('../database');
 const url = require('url');
+const env = require('dotenv').load();
 
+// Get models
+var models = require('../database/models/index.js');
+
+// Sync database
+models.sequelize.sync().then(() => {
+  console.log('Nice! Database looks fine.');
+}).catch((err) => {
+  console.log('Uh oh. something went wrong when updating the database.');
+});
+
+// Create app
 let app = express();
 
+// Setup body parser
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
+// Setup passport and sessions
+app.use(session({
+  secret: 'thisisasecret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // Persistent login sessions
+
+// Serve static files
 app.use(express.static(__dirname + '/../client/dist'));
 
 
@@ -27,4 +52,3 @@ let port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
