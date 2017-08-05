305efe171d11527bf9cc36e57d4bfeda082993e0
Thomas Volk <tvolk131@gmail.com>
2017-08-05 11:23:20 -0700

    Client messages update relative time automatically

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import time from '../helpers/time.js';
 
 class Message extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
+    this.state = {
+      timestampMessage: ''
+    }
+    this.updateTimestamp = this.updateTimestamp.bind(this);
   }
 
-  render() {
+  componentDidMount () {
+    this.updateTimestamp();
+    this.interval = setInterval(this.updateTimestamp, 250);
+  }
+
+  updateTimestamp () {
+    this.setState({timestampMessage: time.parse(this.props.timestamp, true)});
+  }
+
+  render () {
     return (
       <div className='message'>
         <div>{this.props.user}</div>
         <div>{this.props.text}</div>
-        <div>{time.parse(this.props.timestamp, true)}</div>
+        <div>{this.state.timestampMessage}</div>
       </div>
     ) 
   }
