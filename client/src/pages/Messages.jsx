b7ac39d1dd8138193f8dc3059f5726be66715d69
Thomas Volk <tvolk131@gmail.com>
2017-08-05 10:59:57 -0700

    Client messages page uses query parameters to determine the other user when retrieving messages

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -12,6 +12,7 @@ class Messages extends React.Component {
       messages: []
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.fetchMessages(); // Loads messages on initial load or redirect from messages with anothe user
   }
 
   handleInputChange (property, e) {
@@ -20,21 +21,31 @@ class Messages extends React.Component {
     this.setState(stateChange);
   }
 
-  // Fetches the messages between you and the other person
-  fetchMessagesWithUser(username) {
-    var setMessages = (messageArray) => {
-      this.setState({messages: messageArray});
-    }
+  // Fetches the messages between you and another person using your
+   // URL's 'user' query parameter to choose who use as the other person
+  fetchMessages() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var username = urlParams.get('user');
+    if (username) {
+      var setMessages = (messageArray) => {
+        this.setState({messages: messageArray});
+      }
 
-    return axios.get('/api/messages?user=' + username)
-    .then((data) => {
-      return data.data;
-    })
-    .then((messages) => {
-      console.log(messages);
-      setMessages(messages);
-    });
+      return axios.get('/api/messages?user=' + username)
+      .then((data) => {
+        return data.data;
+      })
+      .then((messages) => {
+        console.log(messages);
+        setMessages(messages);
+      });
+    }
   }
+
+  changeUserSelected (username) {
+    window.location.href = '/messages?user=' + username;
+  }
+
   sendMessage(username, text) {
     return axios.post('/api/messages', {username, text}).then(console.log);
   }
@@ -44,6 +55,8 @@ class Messages extends React.Component {
       <div>
         <Navbar/>
         <div>Messages</div>
+        <input type='text' value={this.state.senderName} onChange={this.handleInputChange.bind(this, 'senderName')} /><br/>
+        <button onClick={this.changeUserSelected.bind(this, this.state.senderName)}>Fetch Messages</button><br/>
         <div className='message-list'>
           {this.state.messages.map((message, index) => {
             return <Message user={message.sender} text={message.text} timestamp={message.createdAt} key={index} />
@@ -51,8 +64,6 @@ class Messages extends React.Component {
         </div>
         <input type='text' value={this.state.pendingMessage} onChange={this.handleInputChange.bind(this, 'pendingMessage')} /><br/>
         <button onClick={this.sendMessage.bind(this, this.state.senderName, this.state.pendingMessage)}>Send</button><br/>
-        <input type='text' value={this.state.senderName} onChange={this.handleInputChange.bind(this, 'senderName')} /><br/>
-        <button onClick={this.fetchMessagesWithUser.bind(this, this.state.senderName)}>Fetch Messages</button><br/>
       </div>
     ) 
   }
